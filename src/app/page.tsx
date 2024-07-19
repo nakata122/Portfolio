44833bfe6c3e71964ffbe27551b0e27155dd89bd
nakata122 <redspot122@gmail.com>
2024-07-20 01:24:16 +0300

    Threejs running

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
+'use client'
+import { Canvas } from "@react-three/fiber";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello there 3</h1>
-
-    </main>
+    <div>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
   );
 }
